cee7935178744663ca80987f9e4ff55ea7d48fab
Luan Duarte <luansouza789@gmail.com>
2021-04-24 20:49:53 -0300

    finish challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,39 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!!newTaskTitle){
+      const newTask = {
+        id: Math.floor(Math.random() * (10000000 - 0 + 1)) + 0,
+        title: newTaskTitle,
+        isComplete: false
+      }
+
+      setTasks([...tasks, newTask]);
+
+      setNewTaskTitle('');
+    }
+
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+      if(task.id === id){
+        return {
+          ...task,
+          isComplete: !task.isComplete
+        }
+      }else{
+        return task;
+      }
+    })
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksNotBeDeleted = tasks.filter(task => task.id !== id );
+
+    setTasks(tasksNotBeDeleted);
   }
 
   return (
